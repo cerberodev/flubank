6caa817495a2197714feb0566fd56ae1f1d8aa9e
Pierre Guillen <hi@cerbero.dev>
2020-11-05 10:09:31 -0500

    Adding Button Bar

---
+++ lib/ui/screens/barscreen/barscreen1.dart
@@ -0,0 +1,33 @@
+import 'package:flubank/ui/screens/home/widgets/listcarditem.dart';
+import 'package:flubank/ui/widgets/amount.dart';
+import 'package:flutter/material.dart';
+
+class BarScreenFirst extends StatelessWidget {
+  const BarScreenFirst({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          flex: 1,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0),
+            child: Amount(
+              'Hola Pierre, tu saldo disponible es:',
+              '100',
+              '\$',
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 3,
+          child: Container(
+            width: double.infinity,
+            child: ListCard(),
+          ),
+        )
+      ],
+    );
+  }
+}
