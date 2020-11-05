68c6348ce525a63663bd45f39dcce8dca681548c
Ricardo Rodriguez Bareno <rirodriguez@personalsoft.com.co>
2020-11-05 15:09:30 -0600

    Ajustes de listado de descuentos

--- lib/ui/screens/barscreen/barscreen2.dart
+++ lib/ui/screens/barscreen/barscreen2.dart
@@ -6,7 +6,67 @@ class BarScreenSecond extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(child: Text('Offerts List')),
+      padding: EdgeInsets.all(2),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Text(
+                'Lista de ofertas:',
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(18),
+            width: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10), color: Colors.grey[50],
+              // gradient: LinearGradient(colors: [
+              //   items[index].startColor,
+              //   items[index].endColor
+              // ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey[300],
+                  blurRadius: 12,
+                  offset: Offset(0, 6),
+                ),
+              ],
+            ),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                FlutterLogo(
+                  size: 100,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      'Lista de ofertas:',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      'Lista de ofertas:',
+                      style:
+                          TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            // margin: ,
+          ),
+        ],
+      ),
     );
   }
 }
